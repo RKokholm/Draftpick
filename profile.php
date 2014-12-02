70ce13b56362fd803530320ef981544c7c20f8ab
Rasmus <KokholmGames@hotmail.com>
2014-12-02 22:53:02 +0100

    commit dat shiat

--- profile.php
+++ profile.php
@@ -52,7 +52,7 @@
 			
 
 			?>
-			<span class="user"><?=$username;?></span>
+			<span class="username"><?=$username;?></span>
 			<span class="rank"><?=getMyRank($rank);?></span>
 
 			<div class="underline"></div>
--- userlist.php
+++ userlist.php
@@ -1,17 +1,21 @@
 <?php include_once('includes/header.php'); ?>
 
+	
+<ul>	
+
 	<?php
 
-		$query_users = mysql_query("SELECT * FROM users");
-		
-		 while ($row = mysql_fetch_assoc($query_users)){
-		 	echo $username = $row['username'];
-		 	echo $password = $row['password'];
-		 	echo $rank = $row['rank'];
+		$query_users = mysql_query("SELECT id, username, rank FROM users");
+	
+		while ($row = mysql_fetch_assoc($query_users)){
+			?><li class="list">
+				<div class="username"><?=$row['username'];?></div>
+				<div class="rank"><?=$row['rank'];?></div>
+			</li><?php
 		 }
 
-
-
 	?>
+
+</ul>
 
 <?php include_once('includes/footer.php'); ?>
