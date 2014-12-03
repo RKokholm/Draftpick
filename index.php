ca6ca7b2e61b53b88f4eb9fb1d711b59cfc6a50f
Rasmus <Rasmus-moeller@hotmail.com>
2014-12-03 13:43:46 +0100

    asdasd

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php include_once('includes/header.php'); ?>
 
-<a href="userlist.php">User list</a>
+<h2>Welcome to</h2>
+<h1>Draftpick</h1>
+
+<a class="userlist_link" href="userlist.php">User list</a>
 
 <?php include_once('includes/footer.php'); ?>
--- userlist.php
+++ userlist.php
@@ -1,16 +1,54 @@
 <?php include_once('includes/header.php'); ?>
 
-	
-<ul>	
+	<div class="userlist_titles">
+		<div class="userlist_title"><span>Username</span></div>
+		<div class="userlist_title"><span>Unknown</span></div>
+		<div class="userlist_title"><span>Rank Status</span></div>
+	</div>
+
+<ul id="userlist">	
 
 	<?php
 
 		$query_users = mysql_query("SELECT id, username, rank FROM users");
+
+			function getMyRank($rank) {
+				switch ($rank) {
+					case '1':
+						return 'Advanced Member';
+						break;
+
+					case '2':
+						return 'Moderator';
+						break;
+
+					case '3':
+						return 'Administrator';
+						break;
+					
+					default:
+						return 'Basic Member';
+						break;
+				}
+			}
+
+		$i = 0;
 	
 		while ($row = mysql_fetch_assoc($query_users)){
-			?><li class="list">
-				<div class="username"><?=$row['username'];?></div>
-				<div class="rank"><?=$row['rank'];?></div>
+
+			if ($i == 1) {
+				$i = 0;
+			} else {
+				$i++;
+			}
+			
+			$rank = $row['rank'];
+			$id = $row['id'];
+
+			?><li class="list <?php if($i == 1) echo "style"; ?>">
+				<div class="username"><a href="profile.php?pid=<?=$id;?>"><?=$row['username'];?></a></div>
+				<div class="unknown">HHEEEEEEJ</div>
+				<div class="rank"><?=getMyRank($rank)?></div>
 			</li><?php
 		 }
 
